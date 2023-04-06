2aca0fbe90625bdd55d4cd042c629b4e51a1e708
hyundonk <hyundonk@amazon.com>
2023-04-06 14:40:57 +0900

    initial commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Kim, Hyundong on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
